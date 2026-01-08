c453cf85a79d53996a8ff2383a326fcc37b63db3
Anderson Andrade Silva <ander.math89@gmail.com>
2026-01-07 23:07:09 -0300

    Fim da aula 8 consumo api get

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-// git remote add origin https://github.com/ProfAndersonAndrade/htmx_tail.git
-// git branch -M main
-// git push -u origin main
 import express from 'express'
 import cors from 'cors'
 
@@ -10,18 +7,30 @@ const port = 3333;
 app.use(cors());
 
 app.use(express.static("public"));
-// COnseguir enviar e pegar dados na requisição num FORM
+// Conseguir enviar e pegar dados na requisição num FORM
 app.use(express.urlencoded({extended: true}));
 
 app.use(express.json());
 
-app.get('/users', (req, res)=>{
-    res.send('OK FUNCIONANDO!')
+// app.get('/users', (req, res)=>{
+//     console.log('Requisição foi chamada...')
+//     res.send('<div><h2 style="color: red;">OK FUNCIONANDO!</h2></div>')
+// });
+
+app.get('/users', async (req, res)=>{
+    
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    
+    let htmlResponse = data.map((user) => `<div>${user.name} - ${user.email}</div>`).join("")
+    //console.log(htmlResponse);
+
+    res.send(htmlResponse);
 });
 
 // npm run dev
 app.listen(port, ()=>{
-    console.log(`Backend rodando: http://localhost:${port}`)
+    console.log(`Backend rodando: http://localhost:${port}`);
 });
 
 
